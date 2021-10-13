1d1937d12649d6e3b717c383a9c3f14eba5887cb
Jorge <chauxjorge@hotmail.com>
2021-10-13 11:01:42 -0500

    Add ejemplos de test, probando claidad de productos en example.spec.js

--- vuedex/tests/unit/example.spec.js
+++ vuedex/tests/unit/example.spec.js
@@ -1,14 +1,16 @@
-import { mount } from '@vue/test-utils';
-import App from '@/App.vue';
+// import { mount } from '@vue/test-utils';
+//import App from '@/App.vue';
 import { huevo, espinaca } from './jestTest';
 
-const wrapper = mount(App)
+// const wrapper = mount(App)
 
-console.log(wrapper)
+// console.log(wrapper)
 
-const vm = wrapper.vm
+// const vm = wrapper.vm
 
-console.log("Data de vm === ", vm.$data)
+//console.log("Data de vm === ", vm.$data)
+
+// =========================================================
 /*
 describe(param1, param2)
     param1: tipo string, nombre de nueestro set de pruebas (ojo es set de pruebas, no la peruba en si)
@@ -23,17 +25,28 @@ describe(param1, param2)
 /* Matchers son funciones que nos permite probar el codigo de manera diferente
 - Todos los matchers empiezan con la palabra "expect"
 - Hay multiples matchers pero aquí solo usaremos ".toBe" y ".toEqual"
-
 */
 
-describe('Se probará la calidad de los producotos', () => {
+/*
+- .toBe Compara los valores primitivos. Utiliza la funcion "Object.is" para hacer las validaciones,
+  la cual es mucho mas confiable que un ===
+  
+- .toEqual Compara recursivamente los valores de las propiedades de la instancia del objeto enviado
+  (esto se conoce como "deep quality"). Utiliza de igual manera la funcion Object.is para hacer las validaciones
+  
+  */
+
+describe('Se probará la calidad de los productos', () => {
 
   test('Calidad del huevo', () => {
-    
+    expect(huevo().estado[0]).toBe('fresco');
   });
 
   test('Calidad de la espinaca', () => {
-    
+    expect(espinaca()).toEqual({
+      estado: 1,
+      color: "verde"
+    })
   });
 
 });
