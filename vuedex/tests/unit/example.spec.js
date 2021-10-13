6cc3304915a297478e5a17e28e39fb51839d0ccf
Jorge <chauxjorge@hotmail.com>
2021-10-13 10:19:09 -0500

    First steps with tests. Mount, wrapper and vm

--- vuedex/tests/unit/example.spec.js
+++ vuedex/tests/unit/example.spec.js
@@ -1,12 +1,10 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mount } from '@vue/test-utils'
+import App from '@/App.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+const wrapper = mount(App)
+
+console.log(wrapper)
+
+const vm = wrapper.vm
+
+console.log("Data de vm === ", vm.$data)
